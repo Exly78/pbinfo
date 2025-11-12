a1c9fc1fd514fb2144311fb6055de299efa5d2ab
Exly <Exly788@gmail.com>
2025-11-12 08:39:57 +0200

    Update main.cpp

--- Probleme/Problemele_Maine/#4570 NumarulDeCifre1/main.cpp
+++ Probleme/Problemele_Maine/#4570 NumarulDeCifre1/main.cpp
@@ -1,1 +1,18 @@
-//sa imi bag ceva 
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int p = 0, i = 0;
+    while (n > 0) {
+        int cifra = n % 10;
+        if (cifra % 2 == 0)
+            p++;
+        else
+            i++;
+        n /= 10;
+    }
+    cout << p << " " << i;
+    return 0;
+}
