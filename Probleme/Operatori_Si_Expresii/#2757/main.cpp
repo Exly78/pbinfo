3a5d29ac77e144297c2616beb6a2c4c69c82c399
SKI <skimosk38@gmail.com>
2025-11-17 13:31:39 +0200

    uploaded by SKI's Perplexity

--- Probleme/Operatori_Si_Expresii/#2757/main.cpp
+++ Probleme/Operatori_Si_Expresii/#2757/main.cpp
@@ -1,16 +1,19 @@
 #include<iostream>
 using namespace std;
 int main(){
-    long long k;
-    cin>>k;
-    long long pos=0;
-    for(int i=100;i<=999;i++){
-        if(pos+3>=k){
-            string s=to_string(i);
-            cout<<s[k-pos-1];
-            break;
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        long long num;
+        cin>>num;
+        long long seq=1;
+        while(seq*10<=num)seq*=10;
+        while(seq>0){
+            cout<<num/seq;
+            if(seq>1)cout<<" ";
+            num%=seq;
+            seq/=10;
         }
-        pos+=3;
     }
     return 0;
 }
