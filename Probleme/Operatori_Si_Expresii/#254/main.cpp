104e0b699c13189e83731d9648d05e9b4fe2fca7
SKI <skimosk38@gmail.com>
2025-11-17 12:07:30 +0200

    uploaded by SKI's Perplexity

---
+++ Probleme/Operatori_Si_Expresii/#254/main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        long long a,b;
+        cin>>a>>b;
+        if(a>b)swap(a,b);
+        long long pow2=1;
+        while(pow2*2<=b)pow2*=2;
+        if(pow2>=a)cout<<pow2<<"\n";
+        else cout<<"-1\n";
+    }
+    return 0;
+}
