e044414a4f36aad7794e1be65576a290903b2279
SKI <skimosk38@gmail.com>
2025-11-17 13:34:19 +0200

    uploaded by SKI's Perplexity

--- Probleme/Operatori_Si_Expresii/#3806/main.cpp
+++ Probleme/Operatori_Si_Expresii/#3806/main.cpp
@@ -4,9 +4,18 @@ using namespace std;
 int main(){
     double a,b,c;
     cin>>a>>b>>c;
-    double ma=sqrt((2*b*b+2*c*c-a*a)/4);
-    double mb=sqrt((2*a*a+2*c*c-b*b)/4);
-    double mc=sqrt((2*a*a+2*b*b-c*c)/4);
-    cout<<(long long)ma<<" "<<(long long)mb<<" "<<(long long)mc;
+    if(a+b>c && a+c>b && b+c>a){
+        double ma=sqrt((2*b*b+2*c*c-a*a)/4.0);
+        double mb=sqrt((2*a*a+2*c*c-b*b)/4.0);
+        double mc=sqrt((2*a*a+2*b*b-c*c)/4.0);
+        long long result=(long long)(ma*100+0.5)/100.0;
+        cout<<(long long)result<<"."<<(long long)(ma*100+0.5)%100<<"\n";
+        result=(long long)(mb*100+0.5)/100.0;
+        cout<<(long long)result<<"."<<(long long)(mb*100+0.5)%100<<"\n";
+        result=(long long)(mc*100+0.5)/100.0;
+        cout<<(long long)result<<"."<<(long long)(mc*100+0.5)%100<<"\n";
+    } else {
+        cout<<"Imposibil\n";
+    }
     return 0;
 }
